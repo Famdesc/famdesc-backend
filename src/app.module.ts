74e2e5ca0678db9cc8b9636095f68805055bf584
Osmel Martinez <osmeldev@gmail.com>
2024-08-27 14:07:51 -0400

    feat: :sparkles: add prisma as orm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,27 +1,7 @@
-import { Module, ValidationPipe } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { DatabaseModule } from './database/database.module';
-import { UsersModule } from './user/user.module';
-import { AuthModule } from './auth/auth.module';
-import { ConfigModule } from '@nestjs/config';
-import { APP_FILTER, APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
-
+import { Module } from '@nestjs/common';
 @Module({
-  imports: [
-    ConfigModule,
-    DatabaseModule,
-    UsersModule,
-    AuthModule,
-  ],
-  controllers: [AppController],
-  providers: [
-    AppService,
-    {
-        provide: APP_PIPE,
-        // TODO: 'whitelist' help delete from input dto any property not declared but force value for others and skipMissingProperties has not effect
-        useValue: new ValidationPipe({ whitelist: true }),
-    },
-],
+  imports: [],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
