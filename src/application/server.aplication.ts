0a3f404fbe15feedf84123abc42a461afd042c3f
Osmel Martinez <osmeldev@gmail.com>
2024-09-06 14:59:27 -0400

    feat: :sparkles: Configure SwaggerModule to generate documentation

--- src/application/server.aplication.ts
+++ src/application/server.aplication.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express'
 import { AppModule } from './modules/app.module';
 import { Logger } from '@nestjs/common';
+import { DocumentBuilder, OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
 
 export class ServerApplication {
     private readonly host: string = process.env.HOST;
@@ -12,8 +13,26 @@ export class ServerApplication {
     public async run(): Promise<void> {
        const app: NestExpressApplication = await NestFactory.create<NestExpressApplication>(AppModule);
 
+       this.buildAPIDocumentation(app)
+       this.log()
 
        await app.listen(this.port, this.host)
+    }
+
+    private buildAPIDocumentation(app: NestExpressApplication): void {
+        const title      : string = 'Famdesc';
+        const description: string = 'Famdesc API documentation';
+        const version    : string = '1.0.0'
+
+        const options: Omit<OpenAPIObject, 'paths'> = new DocumentBuilder()
+            .setTitle(title)
+            .setDescription(description)
+            .setVersion(version)
+            .build();
+        
+        const document: OpenAPIObject = SwaggerModule.createDocument(app, options);
+
+        SwaggerModule.setup('doc', app, document)
     }
 
     private log(): void {
